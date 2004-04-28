c3aa629ed5e93155f16f7aec6d0da51f5d4b7f54
fdedinec <fdedinec>
2004-04-28 15:59:39 +0000

    Log nearing perfection

--- log_fast.c
+++ log_fast.c
@@ -9,8 +9,47 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include "log_fast.h"
 #include <crlibm.h>
+#include <crlibm_private.h>
+
+/* The prototypes of the second step */
+extern double scs_log_rn(db_number, int);
+extern double scs_log_ru(db_number, int);
+extern double scs_log_rd(db_number, int);
+
+
+
+
+
+/*  Compile-time switches */
+/* These #defines are intended to provide several performance tradeoffs. */
+
+/* switches on various printfs. Default 0 */
+#define DEBUG 0
+
+/* The code for the first step may be shared, or not. Default to 1 (shared)
+   There is a speed/size tradeoff there.
+   Example : sharing costs 30 cycles out of 1300 on a PIV/Linux/gcc3.3 
+   To be cleaned up, probably.
+*/
+#define SHARE_CODE 1
+
+/* The following constant selects which path to take.  Currently, 9
+is optimal, but if we improve the speed of the second step in the
+future, we may want to try 8 or 7, which will trade off speed for the
+first step and % of taking second step
+*/
+#define CONST_FASTPATH 9
+
+/* constants for fast path, do not edit */
+#define MIN_FASTPATH (0.4 + (1<<CONST_FASTPATH))
+#define DRCST_FASTPATH (1. / ((double)(2<<CONST_FASTPATH)) )
+#define RNCST_FASTPATH (1.0000000000000009 +(1 / (((double)(1<<CONST_FASTPATH)) -1.) ))
+
+/* Beware ! This include should come here, because it needs the last #defines !*/
+#include "log_fast.h"
+
+
 /*
  *  1) First reduction: exponent extraction      
  *         E  
@@ -51,47 +90,19 @@
  *
  */
 
-/* The prototypes of the second step */
-extern double scs_log_rn(db_number, int);
-extern double scs_log_ru(db_number, int);
-extern double scs_log_rd(db_number, int);
 
-
-/*  Compile-time switches */
-/* These #defines are intended to provide several performance tradeoffs. */
-
-/* switches on various printfs. Default 0 */
-#define DEBUG 0
-
-/* The code for the first step may be shared, or not. Default to 1 (shared)
-   There is a speed/size tradeoff there.
-   Example : sharing costs 30 cycles out of 1300 on a PIV/Linux/gcc3.3 */
-#define SHARE_CODE 1
-
-/* The following constant selects which path to take.  Currently, 8
-is optimal, but if we improve the speed of the second step in the
-future, we may want to try 8 or 7, which will trade off speed for the
-first step and % of taking second step
-*/
-#define CONST_FASTPATH 9
-
-/* constant for directed rounding, do not edit */
-#define DRCST_FASTPATH (1. / ((double)(2<<CONST_FASTPATH)) )
-
-/* constant for rounding to nearest, do not edit */
-#define RNCST_FASTPATH (1.0000000000000009 +(4. / (((double)(4<<CONST_FASTPATH)) -1.) ))
 
 
 
 #if SHARE_CODE
-static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_number * py, int * pE) {
+
+static void log_quick(double *pres_hi, double *pres_lo, int* prndcstindex, db_number * py, int E) {
    db_number z,y;
-   double ln2_times_E_HI, ln2_times_E_LO, res_hi, res_lo, roundcst;
+   double ln2_times_E_HI, ln2_times_E_LO, res_hi, res_lo;
    double res, P_hi, P_lo;
-   int k, i, E;
+   int k, i;
 
    y = *py;
-   E=*pE;
    
     /* E belongs to {-52-1023 .. 2046-1023} */
     
@@ -113,16 +124,23 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
      * Polynomial evaluation of log(1 + R) 
      */
 
+#if DEBUG
+    printf("z=%1.30f\n",z.d);
+#endif
     res = (poly_log_fast_h[i][DEGREE]).d;
     for(k=DEGREE-1; k>1; k--){
       res *= z.d;
       res += (poly_log_fast_h[i][k]).d;
     }
    
-    if(ABS(ln2_times_E_HI) < ((double)(1<<CONST_FASTPATH))+0.5) {
+    if(ln2_times_E_HI * ln2_times_E_HI < MIN_FASTPATH*MIN_FASTPATH ) {
       
       /* Slow path */
-      roundcst = rncst[i];
+
+#if DEBUG
+      printf("\nSlow path\n");
+#endif
+
       
       /* Multiply S2 by x = P2 */
       Mul12(&P_hi, &P_lo, res, z.d);
@@ -137,32 +155,40 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
       Add22(&res_hi, &res_lo, (poly_log_fast_h[i][0]).d, (poly_log_fast_l[i][0]).d, P_hi, P_lo);
       
       /* REBUILDING */
-      if (!(E==0)) /* This test saves two tests in Add22Cond */
+      if (!(E==0)) {
+	*prndcstindex =i;
 	Add22(&res_hi, &res_lo, ln2_times_E_HI, ln2_times_E_LO, res_hi, res_lo);
+      }
+      else {
+	*prndcstindex =i+8;
+      }
     }
-
     else { /* Fast path */
-      roundcst=RNCST_FASTPATH;
+#if DEBUG
+       printf("\nFast path\n");
+#endif
+      
       res =  (poly_log_fast_h[i][0]).d + z.d*((poly_log_fast_h[i][1]).d + z.d*res);
       
+      *prndcstindex = 16 ;
+
       /* REBUILDING */
       /* As |ln2_times_E_HI| > CONST_FASTPATH and |res| < 0.5 we may use Add22 */
       Add22(&res_hi, &res_lo, ln2_times_E_HI, ln2_times_E_LO, res, 0.0);
+    }
 #if DEBUG
-      printf("\ni=%d    roundcst=%1.20e\n", i , roundcst);
-      printf("\ res=%1.20e\n E=%d\n Eln2HI=%1.20e\n Eln2LO=%1.20e\n \n",res,E,ln2_times_E_HI,ln2_times_E_LO);
+      printf("\n i=%d    roundcst=%1.20e\n", i , roundcst);
+      printf("\n res=%1.20e\n E=%d\n Eln2HI=%1.20e\n Eln2LO=%1.20e\n \n",res,E,ln2_times_E_HI,ln2_times_E_LO);
       printf("\n   reshi=%1.20e\n   reslo=%1.20e\n",res_hi,res_lo);
       printf("\n    reslo*cst=%1.20e\n",res_lo*roundcst);
+      printf("\n    reslo52=%1.20e\n",res_lo*two52.d);
       printf("\n roundcst=%1.20e\n",roundcst);
 #endif /* DEBUG */
-    }
 
     /* exit */
-    *proundcst = roundcst;
     *pres_hi=res_hi;
     *pres_lo=res_lo;
 }
-
 #endif /* SHARE_CODE */
 
 /*************************************************************
@@ -173,7 +199,7 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
  double log_rn(double x){ 
    db_number y;
    double res_hi,res_lo,roundcst;
-   int E;
+   int E,rndcstindex;
 
    E=0;
    y.d=x;
@@ -204,7 +230,8 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
     }
 
 #if SHARE_CODE
-  log_quick(&res_hi, &res_lo, &roundcst, &y, &E);
+  log_quick(&res_hi, &res_lo, &rndcstindex, &y, E);
+  roundcst = rncst[rndcstindex];
 #else
   {
     db_number z;
@@ -212,35 +239,7 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
     double res, P_hi, P_lo;
     int k, i;
 
-    E=0;
-    
-    y.d = x;
-    /* Filter cases */
-  if (y.i[HI_ENDIAN] < 0x00100000){        /* x < 2^(-1022)    */
-    if (((y.i[HI_ENDIAN] & 0x7fffffff)|y.i[LO_ENDIAN])==0){
-      return 1.0/0.0;     
-    }                    		   /* log(+/-0) = -Inf */
-    if (y.i[HI_ENDIAN] < 0){ 
-      return (x-x)/0;                      /* log(-x) = Nan    */
-    }
-    /* Subnormal number */
-    E = -52; 		
-    y.d *= two52.d; 	  /* make x as normal number = x's mantissa    */ 
-    }
-    
-  if (y.i[HI_ENDIAN] >= 0x7ff00000){
-    return  x+x;				    /* Inf or Nan       */
-  }
-
-    /* find y.d such that sqrt(2)/2 < y.d < sqrt(2) */
-    E += (y.i[HI_ENDIAN]>>20)-1023;				/* extract the exponent */
-    y.i[HI_ENDIAN] =  (y.i[HI_ENDIAN] & 0x000fffff) | 0x3ff00000;	/* do exponent = 0 */
-    if (y.d > SQRT_2){
-      y.d *= 0.5;
-      E++;
-    }
-
-    /* E belongs to {-52-1023 .. 2046-1023} */
+      /* E belongs to {-52-1023 .. 2046-1023} */
     
     /* find the interval including y.d */
     i = (((y.i[HI_ENDIAN] & 0x001F0000)>>16)-6) ;  /* to know which polynom to evaluate */
@@ -260,16 +259,23 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
      * Polynomial evaluation of log(1 + R) 
      */
 
-    res = (poly_log_fast_h[i][13]).d;
-    for(k=12; k>1; k--){
+#if DEBUG
+    printf("z=%1.30f\n",z.d);
+#endif
+    res = (poly_log_fast_h[i][DEGREE]).d;
+    for(k=DEGREE-1; k>1; k--){
       res *= z.d;
       res += (poly_log_fast_h[i][k]).d;
     }
    
-    if(ABS(ln2_times_E_HI) < ((double)(1<<CONST_FASTPATH))+0.5) {
+    if(ln2_times_E_HI * ln2_times_E_HI < MIN_FASTPATH*MIN_FASTPATH ) {
       
       /* Slow path */
-      roundcst = rncst[i];
+
+#if DEBUG
+      printf("\nSlow path\n");
+#endif
+
       
       /* Multiply S2 by x = P2 */
       Mul12(&P_hi, &P_lo, res, z.d);
@@ -284,25 +290,34 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
       Add22(&res_hi, &res_lo, (poly_log_fast_h[i][0]).d, (poly_log_fast_l[i][0]).d, P_hi, P_lo);
       
       /* REBUILDING */
-      if (!(E==0)) /* This test saves two tests in Add22Cond */
+      if (!(E==0)) {
+	roundcst = rncst[i];
 	Add22(&res_hi, &res_lo, ln2_times_E_HI, ln2_times_E_LO, res_hi, res_lo);
+      }
+      else {
+	roundcst = rncst[i+8] ;
+      }
     }
-
     else { /* Fast path */
+#if DEBUG
+       printf("\nFast path\n");
+#endif
       roundcst=RNCST_FASTPATH;
       res =  (poly_log_fast_h[i][0]).d + z.d*((poly_log_fast_h[i][1]).d + z.d*res);
       
       /* REBUILDING */
       /* As |ln2_times_E_HI| > CONST_FASTPATH and |res| < 0.5 we may use Add22 */
       Add22(&res_hi, &res_lo, ln2_times_E_HI, ln2_times_E_LO, res, 0.0);
+    }
 #if DEBUG
-      printf("\ni=%d    roundcst=%1.20e\n", i , roundcst);
-      printf("\ res=%1.20e\n E=%d\n Eln2HI=%1.20e\n Eln2LO=%1.20e\n \n",res,E,ln2_times_E_HI,ln2_times_E_LO);
+      printf("\n i=%d    roundcst=%1.20e\n", i , roundcst);
+      printf("\n res=%1.20e\n E=%d\n Eln2HI=%1.20e\n Eln2LO=%1.20e\n \n",res,E,ln2_times_E_HI,ln2_times_E_LO);
       printf("\n   reshi=%1.20e\n   reslo=%1.20e\n",res_hi,res_lo);
       printf("\n    reslo*cst=%1.20e\n",res_lo*roundcst);
+      printf("\n    reslo52=%1.20e\n",res_lo*two52.d);
       printf("\n roundcst=%1.20e\n",roundcst);
 #endif /* DEBUG */
-    }
+
   }
   
 #endif /* SHARE_CODE */
@@ -328,8 +343,8 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
  *************************************************************/
  double log_rd(double x){ 
    db_number y;
-   double res_hi,res_lo,roundcst;
-   int E;
+   double res_hi,res_lo,roundcst, delta;
+   int E,rndcstindex;
 
    E=0;
    y.d=x;
@@ -361,7 +376,8 @@ static void log_quick(double *pres_hi, double *pres_lo, double * proundcst, db_n
  
 
 #if SHARE_CODE
-  log_quick(&res_hi, &res_lo, &roundcst, &y, &E);
+  log_quick(&res_hi, &res_lo, &rndcstindex, &y, E);
+  roundcst = rncst[rndcstindex];
 #else
   
 #endif /* SHARE_CODE */ 
