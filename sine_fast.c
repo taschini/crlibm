a02999052c22be7488a44c33eb8244d4c3c9ba8a
fdedinec <fdedinec>
2004-05-06 14:12:09 +0000

    Sine, work in progress

--- sine_fast.c
+++ sine_fast.c
@@ -17,50 +17,68 @@ extern double scs_sin_rn(double);
 extern double scs_sin_ru(double);
 extern double scs_sin_rd(double);
 
-#define DEBUG 0
+#define DEBUG 1
 
 
  
 
 static void cos_fast(double *prhi, double *prlo, double yh, double yl){
-  double yy, t;
+  double yyh,yyl, t;
+  double zh,zl,rh,rl;
   int n, i;
 
+  n = (DEG_COS-6)/2;
 
-  n = 10;
- 
-  yy = yh*yh;
+  yyh = yh*yh;
+  /* TODO check it is OK */
+  yyl = 2*yh*yl;
   
   /*Taylor series */
-  t=yy*(PolyCos[n]).d;
+  t=yyh*(PolyCos[n]).d;
   for(i=n-1; i >= 0; i--){
-    t = yy * (t + (PolyCos[i]).d);
-
+    t = yyh * (t + (PolyCos[i]).d);
   }
 
-  Add12(*prhi, *prlo, 1.0, t);
-}
+  /* degree 4 */
+  /* check the Cond */
+  Add22Cond(&zh,&zl,  Coef4Cos[0].d, Coef4Cos[1].d,  t,0.);
+  Mul22(&rh,&rl,  zh,zl,  yyh,yyl);
+
+  /* degree 2 */ 
+  Add22Cond(&zh,&zl,  -0.5, 0.0,  rh,rl);
+  Mul22(&rh,&rl,  zh,zl,  yyh,yyl);
+
+  /* degree 0 */
+  Add22(prhi, prlo, 1.0, 0.0,  rh,rl);
+} 
 
 
 
 
 
 static void sin_fast(double *prhi, double *prlo, double yh, double yl){
-  double yy, t, rh, rl;
+  double yyh,yyl, t;
+  double zh,zl,rh,rl;
   int n, i;
-  n = 10;
-  
 
-  yy = yh*yh;
+  n = (DEG_SIN-5)/2;
+  
+  yyh = yh*yh;
+  yyl = 2*yh*yl;
   
   /*Taylor series */
-  t=yy*(PolySin[n]).d;
-  for(i=n-1; i >= 0; i--){
-    t = yy * (t + (PolySin[i]).d);
+  t=yyh*(PolySin[n]).d;
+  for(i=n-1; i >= 0; i--){ 
+    t = yyh * (t + (PolySin[i]).d);
   }
 
-  Mul22(&rh, &rl,  t,0.0,  yh, yl);
-  Add22(prhi, prlo,  rh, rl, yh, yl);
+  /* degree 3 */
+  /* check the Cond */
+  Add22Cond(&zh,&zl,  Coef3Sin[0].d, Coef3Sin[1].d,  t,0.);
+  Mul22(&rh,&rl,  zh,zl,  yyh,yyl);
+
+  Add22(&zh,&zl,  1.0, 0.0,  rh, rl);
+  Mul22(prhi, prlo,  zh,zl,  yh, yl);
 }
 
 
@@ -75,7 +93,7 @@ static void sin_fast(double *prhi, double *prlo, double yh, double yl){
 double sin_rn(double x){ 
   double reshi, reslo;
   double kd, yh,yl;
-  double absx;
+  double absx, absyh;
   int k;
   
   if (x<0) 
@@ -88,15 +106,24 @@ double sin_rn(double x){
   if (absx < MIN_RETURN_X_FOR_SINE)
     return x;
 
+  /* Compute k */
   DOUBLE2INT(k, x * invpio2.d);
   kd = (double) k;
 
   /* Argument reduction  by Cody & Waite algorithm */
-  Add12 (yh,yl,  ( x - kd*pio2hi.d),  (-kd*pio2lo.d) ) ;
- 
+  Add12 (yh,yl,  (x - kd*pio2hi.d),  (-kd*pio2lo.d) ) ;
+
 #if DEBUG
-   printf("\nk=%d k&3=%d \t y=%5.10f\n", k, k&3, yh);
+   printf("\nx=%1.8e      k=%d (%d) \t y=%5.10f\n", x, k, k&3, yh);
 #endif
+   if(yh<0) absyh=-yh; else absyh=yh;
+   if((k!=0) && (absyh<MINY_ACCURATE)){
+#if DEBUG
+   printf("SCS!\n");    
+#endif
+    return scs_sin_rn(x);
+  }
+
 
   switch (k&3){
   case 0:
@@ -121,12 +148,12 @@ double sin_rn(double x){
   }
   
   /* ROUNDING TO NEAREST */
-
-  //if(reshi == (reshi + (reslo * (1.0078125)))){	/* 2^-7 = 0.0078125 */
+ 
+  if(reshi == (reshi + (reslo * RND_CST))){	
      return reshi;
-  /* }else{ */
-/*      return scs_sin_rn(y); */
-/*   } */
+  }else{ 
+    return scs_sin_rn(x); 
+  } 
 
 }
 
