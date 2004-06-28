90840b4d14d604c351aad47b7798429708feaf8c
fdedinec <fdedinec>
2004-06-28 15:45:16 +0000

    Faster but still without Payne and Hanek

--- sine_fast.c
+++ sine_fast.c
@@ -3,6 +3,8 @@
 #include <crlibm.h>
 #include <crlibm_private.h>
 #include "sine_fast.h"
+#include "coefpi2.h"
+
 
 extern double scs_sin_rn(double);
 extern double scs_sin_ru(double);
@@ -13,25 +15,134 @@ extern double scs_sin_rd(double);
  
 
 
+ 
+
+static void do_sinCodyWaite(double* reshi, double* reslo, double x, int k) {
+  double kd, yh,yl;
+  double thi, tlo, cahyh_h, cahyh_l; 
+  double sah,sal,cah,cal, yh2, ts, tc;
+
+  kd = (double) k;
+
+  k=(k&127)<<2;
+
+  if(k<=(64<<2)) {  /* sah <= cah */
+    sah=sincosTable[k].d ; /* sin(a), high part */
+    sal=sincosTable[k+1].d; /* sin(a), low part */
+    cah=sincosTable[k+2].d; /* cos(a), high part */
+    cal=sincosTable[k+3].d; /* cos(a), low part */
+  } else { /* cah <= sah */
+    int k1=(128<<2) - k;
+    cah=sincosTable[k1].d ; 
+    cal=sincosTable[k1+1].d;
+    sah=sincosTable[k1+2].d;
+    sal=sincosTable[k1+3].d;
+  }    
+
+    if(k==0)
+      { /* Here we risk a large cancellation on yh+yl; on the other hand we have sa=0 and ca=1*/
+	double th,tl;
+	/* all this is exact */
+  	Add12 (th,tl,  (x - kd*pio256hi.d),  (kd*mpio256med1.d) ) ;
+	/* error on the last multiplication, and on the Add22 */
+	Add22 (&yh, &yl, th, tl, kd*mpio256med2.d, kd*mpio256lo2.d) ;
+	yh2 = yh*yh;
+	ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
+	/* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
+	/* Now we need to compute (1+ts)*(yh+yl) */
+	Add12(*reshi,*reslo,   yh, yl+ ts*yh);
+      }
+    else {
+      /* Argument reduction  by Cody & Waite algorithm */
+      /* all this is exact but the rightmost multiplication */
+      Add12 (yh,yl,  (x - kd*pio256hi.d),  (kd*mpio256lo.d) ) ;
+      /* Now y_h is in -Pi/512, Pi/512 */
+      
+      Mul12(&cahyh_h,&cahyh_l, cah, yh);
+
+      yh2 = yh*yh ;
+
+      Add12(thi, tlo,     sah,cahyh_h);
+
+      ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
+      /* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
+       
+      tc = yh2 * (c2.d + yh2*(c4.d + yh2*c6.d ));
+      /* 1+ tc is an approx to cos(yh+yl) */
+      /* now we compute an approximation to cos(a)sin(x) + sin(a)cos(x)   */
+      /* read the sine and cos */ 
+      
+      Add12(*reshi,*reslo, thi,    tc*sah + (ts*cahyh_h  +(sal + (tlo + (cahyh_l  + (cal*yh + cah*yl)) ) ) )  );
+    }
+}
+
+
+
+
+
+static void do_cosCodyWaite(double* reshi, double* reslo, double x, int k) {
+  double kd, yh,yl;
+  double sah,sal,cah,cal, yh2, ts, tc;
+  double thi, tlo, sahyh_h,sahyh_l; 
+
+  kd = (double) k;
+  k=(k&127)<<2;
+
+  if(k<=(64<<2)) {  /* sah <= cah */
+    sah=sincosTable[k].d ; /* sin(a), high part */
+    sal=sincosTable[k+1].d; /* sin(a), low part */
+    cah=sincosTable[k+2].d; /* cos(a), high part */
+    cal=sincosTable[k+3].d; /* cos(a), low part */
+  } else { /* cah <= sah */
+    int k1=(128<<2) - k;
+    cah=sincosTable[k1].d ; 
+    cal=sincosTable[k1+1].d;
+    sah=sincosTable[k1+2].d;
+    sal=sincosTable[k1+3].d;
+  }    
+
+    /* Argument reduction  by Cody & Waite algorithm */
+    /* all this is exact but the rightmost multiplication */
+    Add12 (yh,yl,  (x - kd*pio256hi.d),  (kd*mpio256lo.d) ) ;
+    
+    /* Now y_h is in -Pi/512, Pi/512 */
+    yh2 = yh*yh ;
+
+    /* now we compute an approximation to cos(a)cos(x) - sin(a)sin(x)   */
+       
+    Mul12(&sahyh_h,&sahyh_l, sah, yh);
+
+    ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
+    /* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
+    tc = yh2 * (c2.d + yh2*(c4.d + yh2*(c6.d)));
+    /* 1+ tc is an approx to cos(yh+yl) */
+
+    Add12(thi, tlo,  cah, -sahyh_h);
+    Add12(*reshi, *reslo, thi,    tc*cah - (ts*sahyh_h -  (cal + (tlo  - (sahyh_l + (sal*yh + sah*yl)) )))   );
+}
+
+
 
 /*************************************************************
  *************************************************************
  *               ROUNDED  TO NEAREST			     *
  *************************************************************
- *************************************************************/
+ *************************************************************/ 
 
 double sin_rn(double x){ 
-  double reshi, reslo, absx;
-  double kd, yh,yl;
+  double reshi, reslo, yh, ts;
   int k,quadrant;
-  double sah,sal,cah,cal, yh2, ts, tc;
-  
-  absx=ABS(x);
-  /* TODO remplacer les tests sur absx par des tests sur HI(x) */
+  int absxhi;
+  db_number xx;
 
-#if 1
-  if (absx < ((1./16.))){
-    if (absx <MIN_RETURN_X_FOR_SINE)
+  double rnconstant=1.0002;
+
+
+  xx.d=x;
+  absxhi = xx.i[HI_ENDIAN] & 0x7fffffff;
+
+  if (absxhi < XMAX_SIN_FAST){
+    if (absxhi <XMAX_RETURN_X_FOR_SIN)
       return x;
     /*  fast polynomial */
   
@@ -42,133 +153,44 @@ double sin_rn(double x){
     if(reshi == (reshi + (reslo * RND_CST_SINFAST))){	
       return reshi;
     }else{ 
-#if DEBUG
+#if DEBUG 
       printf("SCS!\n");    
 #endif
       return scs_sin_rn(x); 
     } 
   }
 
-#endif
     
-  if (absx > MAX_FAST_SIN)
+  if (absxhi > XMAX_CODY_WAITE)
     return scs_sin_rn(x);
   
   /* Compute k */
   DOUBLE2INT(k, x * invpio256.d);
-  kd = (double) k;
   quadrant = (k>>7)&3;
   
-  /* Argument reduction  by Cody & Waite algorithm */
+  if (quadrant&1)   /*compute the cos  */
+    do_cosCodyWaite(&reshi, &reslo,  x,  k);
+  else /* compute the sine */
+    do_sinCodyWaite(&reshi, &reslo,  x,  k);
   
-  /* all this is exact but the rightmost multiplication */
-  Add12 (yh,yl,  (x - kd*pio256hi.d),  (-kd*pio256lo.d) ) ;
-
-  /* Now y_h is in -Pi/512, Pi/512 */
-    
-
+  
+  if(quadrant>=2) {
+    reshi = -reshi;
+    reslo = -reslo;
+  }
+  
 #if DEBUG
-  printf("\nx=%1.30e      k=%d (%d)\n yh=%1.30e  yl=%1.30e \n", x, k, k&3, yh,yl);
-#endif
-  
-
-  k=(k&127)<<2;
-  
-
-/* read the sine and cos */ 
-
-  if(k<=(64<<2)) {
-    sah=sincosTable[k].d ; /* sin(a), high part */
-    sal=sincosTable[k+1].d; /* sin(a), low part */
-    cah=sincosTable[k+2].d; /* cos(a), high part */
-    cal=sincosTable[k+3].d; /* cos(a), low part */
-  } else {
-    k=(128<<2) - k;
-    cah=sincosTable[k].d ; 
-    cal=sincosTable[k+1].d;
-    sah=sincosTable[k+2].d;
-    sal=sincosTable[k+3].d;
-  }    
-
-#if DEBUG
+  printf("\nx=%1.30e      k=%d (%d)\n yh=%1.30e  yl=%1.30e \n", x, k, k&127, yh,yl);
 	printf("quadrant=%d  \n", quadrant);
 	printf("sah=%1.30e sal=%1.30e  \n", sah,sal);
 	printf("cah=%1.30e cal=%1.30e  \n", cah,cal);
 	printf("ts=%1.30e tc=%1.30e  \n", ts,tc);
 #endif
-  if (quadrant&1) { 
-    double thi, tlo, sahyh_h,sahyh_l; 
-    /*compute the cos  */
-    yh2 = yh*yh ;
-    ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
-    /* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
-    tc = yh2 * (c2.d + yh2*(c4.d + yh2*(c6.d)));
-    /* 1+ tc is an approx to cos(yh+yl) */
-
-    /* now we compute an approximation to cos(a)cos(x) - sin(a)sin(x)   */
-    
-    /* cos(a)cos(x) ~ (cah+cal)*(1+tc) ~ cah + tc*cah + cal (in magnitude order) */
-    /* sin(a)sin(x) ~ (sah+sal)*(yh+yl)*(1+ts)  
-       ~  sah*yh + sah*yh*ts + sal*yh + sah*yl 
-       + (sal*yh*ts + sah*yl*ts) + sal*yl*ts
-       the 3 last terms may be neglected  */
-      
- 
-    Mul12(&sahyh_h,&sahyh_l, sah, yh);
-    Add12(thi, tlo,   -sahyh_h,   tc*cah - (ts*sah*yh + (sahyh_l + (sal*yh + sah*yl)) )   );
-    /* TODO : check Condition true except when k=0 */ 
-    Add22(&reshi,&reslo,  cah,cal, thi, tlo);
-  }
-
-
-  else { /* compute the sine */
-
-    if(k==0)
-      { /* Here we risk a large cancellation on yh+yl; on the other hand we have sa=0 and ca=1*/
-	double th,tl;
-	/* all this is exact */
-	Add12 (th,tl,  (x - kd*pio256hi.d),  (-kd*pio256med1.d) ) ;
-	/* error on the last multiplication, and on the Add22 */
-	Add22 (&yh, &yl, th, tl, -kd*pio256med2.d, -kd*pio256lo2.d) ;
-
-	yh2 = yh*yh;
-	ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
-	/* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
-	/* Now we need to compute (1+ts)*(yh+yl) */
-	Add12(reshi,reslo, yh, yl+ ts*yh);
-      }
-    else {
-      double thi, tlo, cahyh_h, cahyh_l; 
-      yh2 = yh*yh ;
-      ts = yh2 * (s3.d + yh2*(s5.d + yh2*s7.d));
-      /* (1+ts)*(yh+yl) is an approx to sin(yh+yl) */
-      
-      tc = yh2 * (c2.d + yh2*(c4.d + yh2*(c6.d)));
-      /* 1+ tc is an approx to cos(yh+yl) */
-      /* now we compute an approximation to cos(a)sin(x) + sin(a)cos(x)   */
-      
-      /* sin(a)cos(x) ~ (sah+sal)*(1+tc) ~ sah + tc*sah + sal (in magnitude order) */
-      /* cos(a)sin(x) ~ (cah+cal)*(yh+yl)*(1+ts)  
-	 ~  cah*yh + cah*yh*ts + cal*yh + cah*yl 
-	 + (cal*yh*ts + cah*yl*ts) + cal*yl*ts
-	 the 3 last terms may be neglected  */
-      Mul12(&cahyh_h,&cahyh_l, cah, yh);
-      Add12(thi, tlo,   cahyh_h,   tc*sah + (ts*cah*yh + (cahyh_l + (cal*yh + cah*yl)) )   );
-      /* sacy>casy except when k=0, but then the Add22 works as well */ 
-      Add22(&reshi,&reslo, sah,sal ,   thi, tlo);
-    }
-  }
-
-    if(quadrant>=2) {
-      reshi *= -1;
-      reslo *= -1;
-    }
-
 
 
   /* ROUNDING TO NEAREST */
 
-  if(reshi == (reshi + (reslo * 1.0002))){	
+  if(reshi == (reshi + (reslo * rnconstant))){	
      return reshi;
   }else{ 
 #if DEBUG
