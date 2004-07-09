adc54359cf4573fb6eb79bd5b3c2f7445cfb1d42
fdedinec <fdedinec>
2004-07-09 12:23:44 +0000

    small bug that removed the exp corrected

--- tests/test_common.c
+++ tests/test_common.c
@@ -213,8 +213,7 @@ void test_init(/* pointers to returned value */
       *testfun_mpfr   = mpfr_exp;
 #endif
     }
-
-  if (strcmp (func_name, "atan") == 0)
+  else if (strcmp (func_name, "atan") == 0)
     {
       *randfun        = rand_generic;
       *worst_case= .75417527749959590085206221024712557043923055744016892276704311370849609375e-9;
