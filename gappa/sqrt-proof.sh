d1449d1b572d8ffd27036a501bc0965f177a73dd
clauter <clauter>
2006-01-18 15:11:03 +0000

    Added Gappa proof for sqrt13 macro.

--- gappa/sqrt-proof.sh
+++ gappa/sqrt-proof.sh
@@ -3,4 +3,8 @@
 
 GAPPA=~/schorle/gappa/src/gappa
 
+echo Proving error bound on sqrt12...
 sed -f sqrt.sed sqrt12.gappa | $GAPPA
+echo Proving error bound on sqrt13...
+sed -f sqrt.sed sqrt13.gappa | $GAPPA
+
